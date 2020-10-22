2b2e1e5e8cb8fca375985c9bab86872d0a6cdecb
WillYallop <wyallop14@gmail.com>
2020-10-22 13:28:02 +0100

    Added og:image

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,12 @@ export default {
       { charset: 'UTF-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'theme_color', content: '#080808' },
-      { hid: 'description', name: 'description', content: 'Discover a wide range of fruity playlists to fuel your day! Discover and promote music with Melody Melon!' }
+      { hid: 'description', name: 'description', content: 'Discover a wide range of fruity playlists to fuel your day! Discover and promote music with Melody Melon!' },
+      {
+        hid: "og:image",
+        property: 'og:image',
+        content: '/ogImage.png'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
